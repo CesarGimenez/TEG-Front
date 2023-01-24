965730f289e0f9e010688022144fa03478692763
Cesar Gimenez <cesargimenez760@gmail.com>
2023-01-23 22:03:44 -0400

    Last changes before presentation

--- src/util/constants.jsx
+++ src/util/constants.jsx
@@ -1,2 +1,2 @@
-export const BASE_API = "https://web-production-2625.up.railway.app";
-// export const BASE_API = "http://localhost:3000";
+// export const BASE_API = "https://web-production-2625.up.railway.app";
+export const BASE_API = "http://localhost:3000";
